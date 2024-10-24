f3e7415fde10bb45147e4bc7a7091ac58bdd3bd2
Sergey87 <serge.techno@gmail.com>
2024-10-24 22:15:45 +0300

    added folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Result from './pages/Result';
+import BuyCredit from './pages/BuyCredit';
+import Navbar from './components/Navbar';
+
 const App = () => {
-  return <></>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/result' element={<Result />} />
+        <Route path='/buy' element={<BuyCredit />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/pages/BuyCredit.jsx
@@ -0,0 +1,5 @@
+const BuyCredit = () => {
+  return <>Bue</>;
+};
+
+export default BuyCredit;
