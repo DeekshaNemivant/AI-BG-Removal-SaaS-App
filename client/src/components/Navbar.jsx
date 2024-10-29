a84883d1338ab4f52cd8254e281790ec0eefc174
Sergey87 <serge.techno@gmail.com>
2024-10-30 02:31:49 +0300

    add context in project

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,21 @@
 import { assets } from '../assets/assets';
 import { Link } from 'react-router-dom';
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
+import { useContext, useEffect } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Navbar = () => {
   const { openSignIn } = useClerk();
 
   const { isSignedIn, user } = useUser();
+
+  const { credit, loadCreditsData } = useContext(AppContext);
+
+  useEffect(() => {
+    if (isSignedIn) {
+      loadCreditsData();
+    }
+  }, [isSignedIn]);
   return (
     <div className='flex items-center justify-between mx-4 py-3 lg:mx-44'>
       <Link to='/'>
@@ -13,7 +23,14 @@ const Navbar = () => {
       </Link>
 
       {isSignedIn ? (
-        <div>
+        <div className='flex items-center gap-2 sm:gap-3'>
+          <button className='flex items-center gap-2 bg-blue-100 px-4 sm:px-7 py-1.5 sm:py-2.5 rounded-full hover:scale-105 transition-all duration-700'>
+            <img className='w-5' src={assets.credit_icon} alt='icon pic' />
+            <p className='text-xs sm:text-sm font-medium text-gray-600'>
+              Credits : {credit}
+            </p>
+          </button>
+          <p className='text-gray-600 max-sm:hidden'>Hi, {user.fullName}</p>
           <UserButton />
         </div>
       ) : (
