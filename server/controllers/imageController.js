814dbb78deaa9fe5ac2e5ace65353a5c1eb9cf10
Sergey87 <serge.techno@gmail.com>
2024-10-31 00:38:19 +0300

    work with backend - 4 part

---
+++ server/controllers/imageController.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import fs from 'fs';
+import FormData from 'form-data';
+import userModel from '../models/userModel';
+
+// controller func to remove bg from image
+const removeBgImage = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { removeBgImage };
